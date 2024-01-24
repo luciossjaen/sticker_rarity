00f051d6b602f0b34cb44ed322f34f242b047c1d
Lucioss Jaen <lucioss2302@googlemail.com>
2024-01-24 16:44:39 +0100

    fixed color calls

--- _compatibility/sticker_recycle_bonus/scripts/FutherAttributeCoreItem.gd
+++ _compatibility/sticker_recycle_bonus/scripts/FutherAttributeCoreItem.gd
@@ -2,7 +2,13 @@ extends "res://mods/Sticker_Recycle_Bonus/scripts/AttributeCoreItem.gd"
 
 func get_template_rarity_color():
 	var attribute:StickerAttribute = load(template_path)
-	var rarity_colours = DLC.mods_by_id["lucioss_rarity_tiers"].get_rarity_colors()
+	var rarity_colours:Dictionary = {
+		Rarity.RARITY_COMMON:Color.black, 
+		Rarity.RARITY_UNCOMMON:Color("225d31"), 
+		Rarity.RARITY_RARE:Color("35379d"),
+		Rarity.RARITY_EPIC:Color("BA55D3"),
+		Rarity.RARITY_LEGENDARY:Color("DAA520")
+	}
 	return rarity_colours[attribute.rarity].to_html()
 
 func get_name()->String:
