56acd6ae949b90dc827416ac24e5264959908ad5
Lucioss Jaen <lucioss2302@googlemail.com>
2024-01-28 12:04:06 +0100

    renamed mod ContextInfo

--- scripts/ModContentInfo.gd
+++ scripts/ModContentInfo.gd
@@ -87,6 +87,7 @@ func remove_rarity_tiers():
 	
 	var party_snap = SaveState.party.get_snapshot()
 
+	# removes custom attributes and rarities from current party
 	for tape in party_snap.player.tapes:
 		for sticker in tape.stickers:
 			if sticker == null:
@@ -113,7 +114,8 @@ func remove_rarity_tiers():
 			sticker["rarity"] = rarity if rarity <= 2 else 2
 				
 	corrections.push_back("~cleaned party tape data")
-			
+		
+	# removes custom attributes and rarities from partners
 	for partner in party_snap.partners.values():
 		for tape in partner.tapes:
 			for sticker in tape.stickers:
@@ -143,6 +145,7 @@ func remove_rarity_tiers():
 	corrections.push_back("~cleaned partner tape data")
 	SaveState.party.set_snapshot(party_snap, 2)
 	
+	# removes custom attributes and rarities from tape storage
 	var tape_collection = SaveState.tape_collection.get_snapshot()
 	for tape in tape_collection.tapes:
 		for sticker in tape.stickers:
@@ -177,36 +180,42 @@ func remove_rarity_tiers():
 	pass
 	
 func update_existing_attribute_rarities():
-	var ap_one = preload("res://data/sticker_attributes/ap_refund_1.tres")
-	#ap_one.rarity = 3
-	var auto_ending = preload("res://data/sticker_attributes/auto_use_round_ending.tres")
-	#auto_ending.rarity = 3
-	var auto_after_attack = preload("res://data/sticker_attributes/auto_use_user_attack.tres")
-	#auto_after_attack.rarity = 3
-
-	var extra_slot = preload("res://data/sticker_attributes/extra_slot.tres")
-	#extra_slot.rarity = 3
 	var extra_hit = preload("res://data/sticker_attributes/extra_hit.tres")
-	#extra_hit.rarity = 3
+	extra_hit.rarity = 2
 	var multitarget = preload("res://data/sticker_attributes/multitarget.tres")
-	#multitarget.rarity = 3
-	var spec_pass_evasion = preload("res://data/sticker_attributes/specialization_passive_evasion.tres")
-	#spec_pass_evasion.rarity = 3
-	var stat_pass_evasion = preload("res://data/sticker_attributes/stat_passive_evasion.tres")
-	#stat_pass_evasion.rarity = 3
+	multitarget.rarity = 2
 	var stat_prio_chance = preload("res://data/sticker_attributes/stat_priority_chance.tres")
-	#stat_prio_chance.rarity = 3
+	stat_prio_chance.rarity = 2
+	stat_prio_chance.weight = 1
+	
+	var ap_one = preload("res://data/sticker_attributes/ap_refund_1.tres")
+	ap_one.rarity = 3
+	var auto_ending = preload("res://data/sticker_attributes/auto_use_round_ending.tres")
+	auto_ending.rarity = 3
+	var auto_after_attack = preload("res://data/sticker_attributes/auto_use_user_attack.tres")
+	auto_after_attack.rarity = 3
+	auto_after_attack.weight = 1
+	var extra_slot = preload("res://data/sticker_attributes/extra_slot.tres")
+	extra_slot.rarity = 3
+	var shared = preload("res://data/sticker_attributes/shared.tres")
+	shared.rarity = 3
+	var spec_pass_evasion = preload("res://data/sticker_attributes/specialization_passive_evasion.tres")
+	spec_pass_evasion.rarity = 3
+	spec_pass_evasion.weight = 1
+	var stat_pass_evasion = preload("res://data/sticker_attributes/stat_passive_evasion.tres")
+	stat_pass_evasion.rarity = 3
+	stat_pass_evasion.weight = 1
 	var use_again = preload("res://data/sticker_attributes/use_again.tres")
-	#use_again.rarity = 3
+	use_again.rarity = 3
 	var use_random = preload("res://data/sticker_attributes/use_random.tres")
-	#use_random.rarity = 3
+	use_random.rarity = 3
 	
 	var compatibility = preload("res://data/sticker_attributes/compatibility.tres")
-	#compatibility.rarity = 4
+	compatibility.rarity = 4
 	var ap_all = preload("res://data/sticker_attributes/ap_refund_all.tres")
-	#ap_all.rarity = 4
-	#ap_all.chance_min = 5
-	#ap_all.chance_max = 10
+	ap_all.rarity = 4
+	ap_all.chance_min = 5
+	ap_all.chance_max = 10
 		
 func add_new_attributes():
 	var attribute_profiles = [
@@ -215,7 +224,8 @@ func add_new_attributes():
 		preload("res://data/sticker_attribute_profiles/pure_passive.tres"),
 		preload("res://data/sticker_attribute_profiles/status.tres"),
 		preload("res://data/sticker_attribute_profiles/pure_passive_attack.tres"),
-		preload("res://data/sticker_attribute_profiles/pure_passive_status.tres")
+		preload("res://data/sticker_attribute_profiles/pure_passive_status.tres"),
+		preload("res://data/sticker_attribute_profiles/summon.tres")
 	]
 	
 	# reads and adds new attributes to their corresponding (mapped) profiles	
@@ -236,7 +246,10 @@ func add_new_attributes():
 	# we also have to add the new rarities to the base of Sticker Recycle Bonus to display the correct rarity in menus
 	if DLC.has_mod("sticker_recycle_bonus", 0):					
 		var stickercoresystem_patch = preload("res://mods/sticker_tiers/_compatibility/sticker_recycle_bonus/scripts/StickerCoreSystem_patch.gd")
+		var attributecoreitem_patch = preload("res://mods/sticker_tiers/_compatibility/sticker_recycle_bonus/scripts/AttributeCoreItem_patch.gd")
 		stickercoresystem_patch.patch()
+		attributecoreitem_patch.patch()
+		pass
 	
 	
 	
