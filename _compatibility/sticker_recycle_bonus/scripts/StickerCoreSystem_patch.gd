b502c818d414a64bc62b7a2913115a7e0e016b96
Lucioss Jaen <lucioss2302@googlemail.com>
2024-01-28 16:58:50 +0100

    improved cross-mod stability

--- _compatibility/sticker_recycle_bonus/scripts/StickerCoreSystem_patch.gd
+++ _compatibility/sticker_recycle_bonus/scripts/StickerCoreSystem_patch.gd
@@ -1,54 +1,57 @@
 static func patch():
-	var script_path = "res://mods/Sticker_Recycle_Bonus/scripts/StickerCoreSystem.gd"
-	var patched_script : GDScript = preload("res://mods/Sticker_Recycle_Bonus/scripts/StickerCoreSystem.gd")
+	var mod_test = File.new()
+	
+	if (mod_test.file_exists("res://mods/Sticker_Recycle_Bonus/scripts/StickerCoreSystem.gd")):
+		var script_path = "res://mods/Sticker_Recycle_Bonus/scripts/StickerCoreSystem.gd"
+		var patched_script = load("res://mods/Sticker_Recycle_Bonus/scripts/StickerCoreSystem.gd")
 
-	if !patched_script.has_source_code():
-		var file : File = File.new()
-		var err = file.open(script_path, File.READ)
+		if !patched_script.has_source_code():
+			var file : File = File.new()
+			var err = file.open(script_path, File.READ)
+			if err != OK:
+				push_error("Check that %s is included in Modified Files"% script_path)
+				return
+			patched_script.source_code = file.get_as_text()
+			file.close()
+
+		var code_lines:Array = patched_script.source_code.split("\n")
+		
+		var code_index = code_lines.find("		var html_color = rare_color if mod_attribute.rarity == BaseItem.Rarity.RARITY_RARE else uncommon_color")
+		if code_index >= 0:
+			# adds epic and legendary colors to sticker core system (mod)
+			code_lines.insert(code_index+1,get_code("choose_effect_colors"))
+			code_lines.remove(code_index)
+		
+		# we have to do it twice to replace both appearances of the color assignment	
+		code_index = code_lines.find("		var html_color = rare_color if mod_attribute.rarity == BaseItem.Rarity.RARITY_RARE else uncommon_color")
+		if code_index >= 0:
+			# adds epic and legendary colors to sticker core system (mod)
+			code_lines.insert(code_index+1,get_code("choose_effect_colors"))
+			code_lines.remove(code_index)
+			
+		code_index = code_lines.find("func get_html_color(effect)->int:")
+		if code_index >= 0:
+			# adds epic and legendary colors to general rarity color fetcher
+			code_lines.insert(code_index+1,get_code("get_effect_colors"))
+			
+		code_index = code_lines.find("		if not upgradable_effects and not exists_onsticker(sticker,attr):")
+		if code_index >= 0:
+			# adds epic and legendary rarity comparison for max attribute types
+			code_lines.insert(code_index+1,get_code("check_additional_rarities"))
+			
+		code_index = code_lines.find("func get_sticker_potential(sticker)->Dictionary:")
+		if code_index >= 0:
+			# adds epic and legendary rarity fetch for attribute count
+			code_lines.insert(code_index+2,get_code("add_ratity_max_attribute_check"))
+
+
+		patched_script.source_code = ""
+		for line in code_lines:
+			patched_script.source_code += line + "\n"
+		var err = patched_script.reload(true)
 		if err != OK:
-			push_error("Check that %s is included in Modified Files"% script_path)
+			push_error("Failed to patch %s." % script_path)
 			return
-		patched_script.source_code = file.get_as_text()
-		file.close()
-
-	var code_lines:Array = patched_script.source_code.split("\n")
-	
-	var code_index = code_lines.find("		var html_color = rare_color if mod_attribute.rarity == BaseItem.Rarity.RARITY_RARE else uncommon_color")
-	if code_index >= 0:
-		# adds epic and legendary colors to sticker core system (mod)
-		code_lines.insert(code_index+1,get_code("choose_effect_colors"))
-		code_lines.remove(code_index)
-	
-	# we have to do it twice to replace both appearances of the color assignment	
-	code_index = code_lines.find("		var html_color = rare_color if mod_attribute.rarity == BaseItem.Rarity.RARITY_RARE else uncommon_color")
-	if code_index >= 0:
-		# adds epic and legendary colors to sticker core system (mod)
-		code_lines.insert(code_index+1,get_code("choose_effect_colors"))
-		code_lines.remove(code_index)
-		
-	code_index = code_lines.find("func get_html_color(effect)->int:")
-	if code_index >= 0:
-		# adds epic and legendary colors to general rarity color fetcher
-		code_lines.insert(code_index+1,get_code("get_effect_colors"))
-		
-	code_index = code_lines.find("		if not upgradable_effects and not exists_onsticker(sticker,attr):")
-	if code_index >= 0:
-		# adds epic and legendary rarity comparison for max attribute types
-		code_lines.insert(code_index+1,get_code("check_additional_rarities"))
-		
-	code_index = code_lines.find("func get_sticker_potential(sticker)->Dictionary:")
-	if code_index >= 0:
-		# adds epic and legendary rarity fetch for attribute count
-		code_lines.insert(code_index+2,get_code("add_ratity_max_attribute_check"))
-
-
-	patched_script.source_code = ""
-	for line in code_lines:
-		patched_script.source_code += line + "\n"
-	var err = patched_script.reload(true)
-	if err != OK:
-		push_error("Failed to patch %s." % script_path)
-		return
 
 
 static func get_code(block_name:String)->String:
